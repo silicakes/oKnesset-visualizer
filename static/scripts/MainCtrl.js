da38ef1be04c3ca704930832cea3f1c7f69a1c25
Michael Katz <Mike.L.Katz@gmail.com>
2014-12-19 14:19:27 +0200

    updated bower dependencies

--- static/scripts/MainCtrl.js
+++ static/scripts/MainCtrl.js
@@ -1,27 +1,28 @@
 (function() {
-  app.controller("MainCtrl", function($scope, $http) {
-    var urlMap = {
-      services: "http://localhost:5556/services/serviceList"
-    };
+	app.controller("MainCtrl", function($scope, $http, $sce) {
+		var urlMap = {
+			services: "http://localhost:5556/services/serviceList"
+		};
 
-    $scope.features;
-    $scope.currentFeature;
+		$scope.features;
+		$scope.currentFeature;
 
-    function featuresRetrieved(data) {
-    	$scope.features = data;
-    }
+		function featuresRetrieved(data) {
+			$scope.features = data;
+		}
 
-    $scope.logResponse = function(service) {
-    	$http.get("http://localhost:5556/services/" + service).success(function(data) {
-    		$scope.currentFeature = data;
-    	});
-    }
-    
-    $scope.services = $http.get(urlMap.services,{
-  		headers: {
-    		'Content-type': 'application/json'
-  		}
+		$scope.logResponse = function(service) {
+			$http.get("http://localhost:5556/services/" + service).success(function(data) {
+				$scope.currentFeature = data;
+			});
+		}
+
+		
+		$scope.services = $http.get(urlMap.services,{
+			headers: {
+				'Content-type': 'application/json'
+			}
 	}).success(featuresRetrieved);
 
-  });
+	});
 }());
